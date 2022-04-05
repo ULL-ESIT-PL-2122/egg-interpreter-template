027d8356ccc8ce0748e863b90f604c7dbf7ec169
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2022-04-05 14:46:25 +0100

    bin

--- bin/egg.js
+++ bin/egg.js
@@ -5,6 +5,4 @@ let {runFromFile}  = require('../src/egg-interpreter.js');
 
 let eggRepl = require('../src/egg-repl.js');
 
-const fileName = process.argv.slice(2).shift();
-if (fileName && (fileName.length > 0) && fs.existsSync(fileName)) runFromFile(fileName);
-else eggRepl(); // else  read evaluate print loop
+/* ... fill the code ... */
